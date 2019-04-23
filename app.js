b2032b722b95a713f4e572f3f9b8959dcf6ac0da
Damian Dzieduch <d.dzieduch@gmail.com>
2019-04-23 15:19:10 +1000

    Add views

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
 const routes = require('./routes/index');
 
 const app = express();
+
+app.use(session({secret: 'sessionSecret'}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use('/', routes);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const session = require('express-session');
 
 const router = express.Router();
 
+let sess
+
 router.get('/', (req, res) => {
-  res.send('It works!');
+  res.render('form');
+});
+
+router.post('/', (req, res) => {
+  sess = req.session;
+  sess.name  = req.body.name;
+  sess.email = req.body.email;
+  res.redirect('result');
+});
+
+router.get('/result', (req, res) => {
+  res.render('result');
 });
 
 module.exports = router;
